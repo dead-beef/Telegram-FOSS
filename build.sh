0a248c04092fb023d21bad9e68aff6633971195b
dead-beef <dead-beef@users.noreply.github.com>
2019-11-05 18:50:25 +0000

    add .editorconfig

--- build.sh
+++ build.sh
@@ -1,84 +1,84 @@
 #!/bin/bash
 
 usage() {
-	cat >&2 <<EOF
+    cat >&2 <<EOF
 usage: $0 [-a]
 
 optional arguments:
   -h    show this help message and exit
   -a    build native dependencies
 EOF
-	exit 1
+    exit 1
 }
 
 cmd() {
-	echo "$@" >&2
-	"$@" || exit 1
+    echo "$@" >&2
+    "$@" || exit 1
 }
 
 build_deps=
 
 while getopts 'ha' OPT; do
-	case "$OPT" in
-		a) build_deps=1 ;;
-		h) usage ;;
-		\?) exit 1 ;;
-	esac
+    case "$OPT" in
+        a) build_deps=1 ;;
+        h) usage ;;
+        \?) exit 1 ;;
+    esac
 done
 
 if [[ -z $JAVA_HOME ]]; then
-	dir="${HOME}/java/"
-	if [[ -d $dir ]]; then
-		export JAVA_HOME="$dir"
-	else
-		echo "Error: JAVA_HOME is not set and ${dir} does not exist" >&2
-		exit 1
-	fi
+    dir="${HOME}/java/"
+    if [[ -d $dir ]]; then
+        export JAVA_HOME="$dir"
+    else
+        echo "Error: JAVA_HOME is not set and ${dir} does not exist" >&2
+        exit 1
+    fi
 fi
 
 if [[ -z $NINJA_PATH ]]; then
-	if [[ -f /usr/bin/ninja ]]; then
-		export NINJA_PATH=/usr/bin/ninja
-	else
-		echo 'Error: NINJA_PATH is not set and /usr/bin/ninja does not exist' >&2
-		exit 1
-	fi
+    if [[ -f /usr/bin/ninja ]]; then
+        export NINJA_PATH=/usr/bin/ninja
+    else
+        echo 'Error: NINJA_PATH is not set and /usr/bin/ninja does not exist' >&2
+        exit 1
+    fi
 fi
 
 if [[ -z $ANDROID_HOME ]]; then
-	dir="${HOME}/Android/Sdk"
-	if [[ -d $dir ]]; then
-		export ANDROID_HOME="$dir"
-	else
-		echo "Error: ANDROID_HOME is not set and ${dir} does not exist" >&2
-		exit 1
-	fi
+    dir="${HOME}/Android/Sdk"
+    if [[ -d $dir ]]; then
+        export ANDROID_HOME="$dir"
+    else
+        echo "Error: ANDROID_HOME is not set and ${dir} does not exist" >&2
+        exit 1
+    fi
 fi
 
 if [[ -z $ANDROID_BUILD_TOOLS ]]; then
-	dir="$(ls "${ANDROID_HOME}/build-tools" | head -n 1)"
-	if [[ -n $dir ]]; then
-		ANDROID_BUILD_TOOLS="${ANDROID_HOME}/build-tools/${dir}"
-	else
-		echo "Error: ANDROID_BUILD_TOOLS is not set and ${ANDROID_HOME}/build-tools/<version> does not exist" >&2
-		exit 1
-	fi
+    dir="$(ls "${ANDROID_HOME}/build-tools" | head -n 1)"
+    if [[ -n $dir ]]; then
+        ANDROID_BUILD_TOOLS="${ANDROID_HOME}/build-tools/${dir}"
+    else
+        echo "Error: ANDROID_BUILD_TOOLS is not set and ${ANDROID_HOME}/build-tools/<version> does not exist" >&2
+        exit 1
+    fi
 fi
 
 if [[ -z $NDK ]]; then
-	dir="$(ls "${ANDROID_HOME}/ndk" | head -n 1)"
-	if [[ -n $dir ]]; then
-		export NDK="${ANDROID_HOME}/ndk/${dir}"
-	else
-		echo "Error: NDK is not set and ${ANDROID_HOME}/ndk/<version> does not exist" >&2
-		exit 1
-	fi
+    dir="$(ls "${ANDROID_HOME}/ndk" | head -n 1)"
+    if [[ -n $dir ]]; then
+        export NDK="${ANDROID_HOME}/ndk/${dir}"
+    else
+        echo "Error: NDK is not set and ${ANDROID_HOME}/ndk/<version> does not exist" >&2
+        exit 1
+    fi
 fi
 
 if [[ -z $KEY ]]; then
-	KEY='release-key.keystore'
-	KEY_ALIAS='key-alias'
-	KEY_PASSWORD='release'
+    KEY='release-key.keystore'
+    KEY_ALIAS='key-alias'
+    KEY_PASSWORD='release'
 fi
 
 BUILD_APK='TMessagesProj/build/outputs/apk/afat/release/TMessagesProj-afat-release-unsigned.apk'
@@ -89,32 +89,32 @@ echo 'Cleaning...' >&2
 rm -fv "$BUILD_APK" "$UNSIGNED_APK" "$SIGNED_APK"
 
 if [[ -n $build_deps ]]; then
-	cmd pushd TMessagesProj/jni
+    cmd pushd TMessagesProj/jni
 
-	echo 'Resetting native dependencies...' >&2
-	cmd pushd boringssl
-	rm -rfv build
-	cmd git checkout -- .
-	cmd popd
-	cmd pushd ffmpeg
-	cmd git checkout -- .
-	cmd popd
+    echo 'Resetting native dependencies...' >&2
+    cmd pushd boringssl
+    rm -rfv build
+    cmd git checkout -- .
+    cmd popd
+    cmd pushd ffmpeg
+    cmd git checkout -- .
+    cmd popd
 
-	echo 'Building native dependencies...' >&2
-	cmd ./build_ffmpeg_clang.sh
-	cmd ./patch_ffmpeg.sh
-	cmd ./patch_boringssl.sh
-	cmd ./build_boringssl.sh
+    echo 'Building native dependencies...' >&2
+    cmd ./build_ffmpeg_clang.sh
+    cmd ./patch_ffmpeg.sh
+    cmd ./patch_boringssl.sh
+    cmd ./build_boringssl.sh
 
-	cmd popd
+    cmd popd
 fi
 
 echo 'Building APK...' >&2
 cmd ./gradlew assembleAfatRelease
 
 if [[ ! -e $KEY ]]; then
-	echo 'Generating key...' >&2
-	cmd keytool -genkey -v -keystore "$KEY" -alias "$KEY_ALIAS" -storepass "$KEY_PASSWORD" -keyalg RSA -keysize 2048 -validity 1000000
+    echo 'Generating key...' >&2
+    cmd keytool -genkey -v -keystore "$KEY" -alias "$KEY_ALIAS" -storepass "$KEY_PASSWORD" -keyalg RSA -keysize 2048 -validity 1000000
 fi
 
 echo 'Signing APK...' >&2
