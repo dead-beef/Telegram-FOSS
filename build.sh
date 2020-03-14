ed37168af00caef17cdaf2becf30ca34e243b932
dead-beef <dead-beef@users.noreply.github.com>
2020-03-14 21:54:19 +0000

    update build script

--- build.sh
+++ build.sh
@@ -2,11 +2,13 @@
 
 usage() {
     cat >&2 <<EOF
-usage: $0 [-a]
+usage: $0 [-a|-d|-j]
 
 optional arguments:
   -h    show this help message and exit
-  -a    build native dependencies
+  -j    build only apk (default)
+  -d    build only native dependencies
+  -a    build native dependencies and apk
 EOF
     exit 1
 }
@@ -17,81 +19,101 @@ cmd() {
 }
 
 build_deps=
+build_apk=1
 
-while getopts 'ha' OPT; do
+while getopts 'hadj' OPT; do
     case "$OPT" in
-        a) build_deps=1 ;;
+        a)
+            build_deps=1
+            build_apk=1
+            ;;
+        d)
+            build_deps=1
+            build_apk=
+            ;;
+        j)
+            build_deps=
+            build_apk=1
+            ;;
         h) usage ;;
         \?) exit 1 ;;
     esac
 done
 
-if [[ -z $JAVA_HOME ]]; then
-    dir="${HOME}/java/"
-    if [[ -d $dir ]]; then
-        export JAVA_HOME="$dir"
-    else
-        echo "Error: JAVA_HOME is not set and ${dir} does not exist" >&2
-        exit 1
+cmd cd "$(dirname "${BASH_SOURCE[0]}")"
+
+if [[ -e vars.sh ]]; then
+    source vars.sh
+fi
+
+if [[ -n $build_apk ]]; then
+    if [[ -z $JAVA_HOME ]]; then
+        dir="${HOME}/java/"
+        if [[ -d $dir ]]; then
+            export JAVA_HOME="$dir"
+        else
+            echo "Error: JAVA_HOME is not set and ${dir} does not exist" >&2
+            exit 1
+        fi
+    fi
+
+    if [[ -z $ANDROID_HOME ]]; then
+        dir="${HOME}/Android/Sdk"
+        if [[ -d $dir ]]; then
+            export ANDROID_HOME="$dir"
+        else
+            echo "Error: ANDROID_HOME is not set and ${dir} does not exist" >&2
+            exit 1
+        fi
+    fi
+
+    if [[ -z $ANDROID_BUILD_TOOLS ]]; then
+        dir="$(ls "${ANDROID_HOME}/build-tools" | head -n 1)"
+        if [[ -n $dir ]]; then
+            ANDROID_BUILD_TOOLS="${ANDROID_HOME}/build-tools/${dir}"
+        else
+            echo "Error: ANDROID_BUILD_TOOLS is not set and ${ANDROID_HOME}/build-tools/<version> does not exist" >&2
+            exit 1
+        fi
+    fi
+
+    if [[ -z $NDK ]]; then
+        dir="$(ls "${ANDROID_HOME}/ndk" | head -n 1)"
+        if [[ -n $dir ]]; then
+            export NDK="${ANDROID_HOME}/ndk/${dir}"
+        else
+            echo "Error: NDK is not set and ${ANDROID_HOME}/ndk/<version> does not exist" >&2
+            exit 1
+        fi
+    fi
+
+    if [[ -z $KEY ]]; then
+        KEY='release-key.keystore'
+        KEY_ALIAS='key-alias'
+        KEY_PASSWORD='release'
     fi
 fi
 
-if [[ -z $NINJA_PATH ]]; then
-    if [[ -f /usr/bin/ninja ]]; then
-        export NINJA_PATH=/usr/bin/ninja
-    else
-        echo 'Error: NINJA_PATH is not set and /usr/bin/ninja does not exist' >&2
-        exit 1
+if [[ -n $build_deps ]]; then
+    if [[ -z $NINJA_PATH ]]; then
+        if [[ -f /usr/bin/ninja ]]; then
+            export NINJA_PATH=/usr/bin/ninja
+        else
+            echo 'Error: NINJA_PATH is not set and /usr/bin/ninja does not exist' >&2
+            exit 1
+        fi
     fi
 fi
 
-if [[ -z $ANDROID_HOME ]]; then
-    dir="${HOME}/Android/Sdk"
-    if [[ -d $dir ]]; then
-        export ANDROID_HOME="$dir"
-    else
-        echo "Error: ANDROID_HOME is not set and ${dir} does not exist" >&2
-        exit 1
-    fi
-fi
-
-if [[ -z $ANDROID_BUILD_TOOLS ]]; then
-    dir="$(ls "${ANDROID_HOME}/build-tools" | head -n 1)"
-    if [[ -n $dir ]]; then
-        ANDROID_BUILD_TOOLS="${ANDROID_HOME}/build-tools/${dir}"
-    else
-        echo "Error: ANDROID_BUILD_TOOLS is not set and ${ANDROID_HOME}/build-tools/<version> does not exist" >&2
-        exit 1
-    fi
-fi
-
-if [[ -z $NDK ]]; then
-    dir="$(ls "${ANDROID_HOME}/ndk" | head -n 1)"
-    if [[ -n $dir ]]; then
-        export NDK="${ANDROID_HOME}/ndk/${dir}"
-    else
-        echo "Error: NDK is not set and ${ANDROID_HOME}/ndk/<version> does not exist" >&2
-        exit 1
-    fi
-fi
-
-if [[ -z $KEY ]]; then
-    KEY='release-key.keystore'
-    KEY_ALIAS='key-alias'
-    KEY_PASSWORD='release'
-fi
 
 BUILD_APK='TMessagesProj/build/outputs/apk/afat/release/TMessagesProj-afat-release-unsigned.apk'
 UNSIGNED_APK='build/release-unsigned.apk'
 SIGNED_APK='build/release-signed.apk'
 
-echo 'Cleaning...' >&2
-rm -fv "$BUILD_APK" "$UNSIGNED_APK" "$SIGNED_APK"
-
 if [[ -n $build_deps ]]; then
     cmd pushd TMessagesProj/jni
 
-    echo 'Resetting native dependencies...' >&2
+    echo 'Cleaning native dependencies...' >&2
     cmd pushd boringssl
     rm -rfv build
     cmd git checkout -- .
@@ -109,16 +131,21 @@ if [[ -n $build_deps ]]; then
     cmd popd
 fi
 
-echo 'Building APK...' >&2
-cmd ./gradlew assembleAfatRelease
+if [[ -n $build_apk ]]; then
+    echo 'Cleaning APK...' >&2
+    rm -fv "$BUILD_APK" "$UNSIGNED_APK" "$SIGNED_APK"
 
-if [[ ! -e $KEY ]]; then
-    echo 'Generating key...' >&2
-    cmd keytool -genkey -v -keystore "$KEY" -alias "$KEY_ALIAS" -storepass "$KEY_PASSWORD" -keyalg RSA -keysize 2048 -validity 1000000
+    echo 'Building APK...' >&2
+    cmd ./gradlew assembleAfatRelease
+
+    if [[ ! -e $KEY ]]; then
+        echo 'Generating key...' >&2
+        cmd keytool -genkey -v -keystore "$KEY" -alias "$KEY_ALIAS" -storepass "$KEY_PASSWORD" -keyalg RSA -keysize 2048 -validity 1000000
+    fi
+
+    echo 'Signing APK...' >&2
+    cmd cp "$BUILD_APK" "$UNSIGNED_APK"
+    cmd jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore "$KEY" "$UNSIGNED_APK" "$KEY_ALIAS" -storepass "$KEY_PASSWORD"
+    cmd "${ANDROID_BUILD_TOOLS}/zipalign" -v 4 "$UNSIGNED_APK" "$SIGNED_APK"
+    cmd "${ANDROID_BUILD_TOOLS}/apksigner" verify "$SIGNED_APK"
 fi
-
-echo 'Signing APK...' >&2
-cmd cp "$BUILD_APK" "$UNSIGNED_APK"
-cmd jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore "$KEY" "$UNSIGNED_APK" "$KEY_ALIAS" -storepass "$KEY_PASSWORD"
-cmd "${ANDROID_BUILD_TOOLS}/zipalign" -v 4 "$UNSIGNED_APK" "$SIGNED_APK"
-cmd "${ANDROID_BUILD_TOOLS}/apksigner" verify "$SIGNED_APK"
